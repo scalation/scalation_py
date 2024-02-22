e321b74d5719d384d299c18747b353b80dc2d3c3
maldosari1 <maa25321@uga.edu>
2024-02-22 15:34:50 -0500

    Updated RW.

--- modeling/statmodeling/RandomWalk.py
+++ modeling/statmodeling/RandomWalk.py
@@ -4,8 +4,10 @@ from util.data_loading import load_data, plot_train_test
 from util.data_transforms import data_transform_std
 from util.data_splitting import train_test_split
 from util.metrics import mse, mae, smape
-from util.getmetrics import getmetrics
-def RandomWalk(file_name: str, training_ratio: float, horizon: int, main_output: str, normalization: bool) -> (int, float, float, float):
+
+
+def RandomWalk(file_name: str, training_ratio: float, horizon: int, main_output: str, normalization: bool) -> (
+int, float, float, float):
     """
     A function used for producing forecasts based on the Random Walk model that simply projects a current value into the future (yhat[t] = y[t-h]).
 
@@ -21,7 +23,7 @@ def RandomWalk(file_name: str, training_ratio: float, horizon: int, main_output:
         the main output column/feature, e.g. '% WEIGHTED ILI'
     normalization: bool
         specifies whether the data is normalized or original
-        
+
     Returned Values
     ----------
     mse: float
@@ -30,23 +32,20 @@ def RandomWalk(file_name: str, training_ratio: float, horizon: int, main_output:
 
     """
     horizon = horizon - 1
-    data = load_data(file_name, main_output = main_output)
-    train_size = int(training_ratio*len(data))
+    data = load_data(file_name, main_output=main_output)
+    train_size = int(training_ratio * len(data))
     if normalization:
         scaled_mean_std, data = data_transform_std(data, train_size)
 
-    train_data, val_data, test_data = train_test_split(data, train_ratio = training_ratio)    # No validation data for Random Walk.   
-    train_data_MO = train_data[[main_output]]                   # Train set for main output column.
-    test_data_MO = test_data[[main_output]]                     # Test set for main output column.
-    actual = data[[main_output]]                                # Actual complete dataset for main output.
-    forecasts = []
+    train_data, val_data, test_data = train_test_split(data,
+                                                       train_ratio=training_ratio)  # No validation data for Random Walk.
+    train_data_MO = train_data[[main_output]]  # Train set for main output column.
+    test_data_MO = test_data[[main_output]]  # Test set for main output column.
+    actual = np.zeros(shape=(len(test_data_MO) - horizon, horizon + 1))  # Actual complete dataset for main output.
+    forecasts = np.zeros(shape=(len(test_data_MO) - horizon, horizon + 1))
     for i in range(len(test_data_MO) - horizon):
-        forecasts.append(actual.iloc[train_size + i - 1,:])     # For Random Walk, start with the last value from the training set.
-
-    actual = actual[train_size + horizon:]                      # Aligning actual with forecasts so both have the same length.
-    actual = np.array(actual)                                   # Convert actual a numpy array
-
+        for j in range(horizon + 1):
+            actual[i, j] = float(data.iloc[train_size + i + j, :][main_output])
+            forecasts[i, j] = float(data.iloc[train_size + i - 1,:][main_output])  # For Random Walk, start with the last value from the training set.
     plot_train_test(data, main_output, train_size, train_data_MO, test_data_MO, forecasts, horizon)
-    mse, mae, smape = getmetrics(actual, forecasts)
-    
-    return len(actual), mse, mae, smape
+    return actual, forecasts
