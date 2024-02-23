f5b21be399ce974a44492d025bd49a9173d5b86e
maldosari1 <maa25321@uga.edu>
2024-02-22 20:12:52 -0500

    Updated notebook.

--- modeling/statmodeling/SimpleMovingAverage.py
+++ modeling/statmodeling/SimpleMovingAverage.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from typing import Any, Union
 from numpy import ndarray, dtype, floating, float_
 from numpy._typing import _64Bit
@@ -38,8 +37,8 @@ def SimpleMovingAverage(file_name: str, training_ratio: float, horizon: int, mai
         scaled_mean_std, data = data_transform_std(data, train_size)
 
     train_data, val_data, test_data = train_test_split(data, train_ratio=training_ratio)  # No validation data for the SMA model.
-    train_data_MO: pd.DataFrame = train_data[[main_output]]  # Train set for main output column.
-    test_data_MO: pd.DataFrame = test_data[[main_output]]  # Test set for main output column.
+    train_data_MO = train_data[[main_output]]  # Train set for main output column.
+    test_data_MO = test_data[[main_output]]  # Test set for main output column.
     actual: ndarray[Any, dtype[Union[floating[_64Bit], float_]]] = np.zeros(shape=(len(test_data_MO) - horizon, horizon + 1))  # Make an initital array for storing the actual values.
     forecasts: ndarray[Any, dtype[Union[floating[_64Bit], float_]]] = np.zeros(shape=(len(test_data_MO) - horizon, horizon + 1))   # Make an initital array for storing the forecasts values.
     for i in range(len(test_data_MO) - horizon):
