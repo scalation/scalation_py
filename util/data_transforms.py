8802128bf56f34285ea30abcdd9f34ef7fb9eb0b
maldosari1 <maa25321@uga.edu>
2024-02-22 22:10:34 -0500

    Added author.

--- util/data_transforms.py
+++ util/data_transforms.py
@@ -1,3 +1,10 @@
+"""
+__author__ = "Mohammed Aldosari"
+__date__ = 2/22/24
+__version__ = "1.0"
+__license__ = "MIT style license file"
+"""
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
--- util/getmetrics.py
+++ util/getmetrics.py
@@ -1,3 +1,10 @@
+"""
+__author__ = "Mohammed Aldosari"
+__date__ = 2/22/24
+__version__ = "1.0"
+__license__ = "MIT style license file"
+"""
+
 import numpy as np
 from util.metrics import mse, mae, smape
 
